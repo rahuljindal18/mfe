c5d7ed9487ec9f3cf54c5ea5bf9c43b14867ee5b
rj1809 <rahuljindal1989@gmail.com>
2022-09-26 16:52:16 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingContainer from "./components/MarketingContainer";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi There!</h1>
+      <hr />
+      <MarketingContainer />
+    </div>
+  );
+};
